3038ed40db9cf90335c52f3d0bd5c4cce1b4704f
Herman <s32370@pjwstk.edu.pl>
2026-04-21 13:47:47 +0200

    empty personal trianing plan added to git

---
+++ src/PersonalTrainingPlan.java
@@ -0,0 +1,6 @@
+public class PersonalTrainingPlan {
+    //call the base class constructor using super(...),
+    //implement getPlanType(),
+    //implement calculateMonthlyNetPrice(),
+    //extend toString() with fields specific to that plan.
+}
